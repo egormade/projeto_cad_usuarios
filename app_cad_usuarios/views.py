d9198c8abefc17f4d9a97cd97f9bdd33712cdac2
Egor <96543667+egormade@users.noreply.github.com>
2023-08-21 11:31:45 -0300

    Initial commit

---
+++ app_cad_usuarios/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Usuario
+
+def home(request):
+    return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    #Salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    #Exibir todos os usuarios ja cadastrados no banco
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    #Retornar os dados para a pagina de listagem de usuarios
+    return render(request, 'usuarios/usuario.html', usuarios)
+
+def delete(request):
+    pass
---
+++ projeto_cad_usuarios/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from app_cad_usuarios import views
+
+
+urlpatterns = [
+
+    # rota, view responsável, nome de referência
+
+    path('', views.home, name='home'),
+    # facebook.com/login
+    # path('login/')
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
+]
+
